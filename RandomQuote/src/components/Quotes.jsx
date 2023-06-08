122d911f927e3b9af31810b3d4749c2a2b5e8a50
Loamy28 <rome.angelo15@gmail.com>
2023-06-08 12:50:05 +0800

    2nd Commit

---
+++ RandomQuote/src/components/Quotes.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react'
+
+const Quotes = () => {
+    const [quote, setQuote] = useState('');
+    const [author, setAuthor] = useState('');
+
+    useEffect(() =>{
+        getQuote()
+    }, []);
+
+    const getQuote = () => {
+        let url = `https://dummyjson.com/quotes`;
+        fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            let dataQuotes = data.quotes;
+            let randomNum = Math.floor(Math.random() * dataQuotes.length);
+            let randomQuote = dataQuotes[randomNum];
+            setQuote(randomQuote.quote);
+            setAuthor(randomQuote.author);
+        })
+    }
+
+    const handleClick = () => {
+        getQuote();
+    }
+
+
+  return (
+    <div id="quote-box">
+        <div id="text"><p>{quote}</p></div>
+        <div id="author"><p>- {author}</p></div>
+
+        <div id="buttons">
+            <div className="social-media">
+
+            </div>
+            <button onClick={handleClick} id="new-quote">New Quote</button>
+        </div>
+    </div>
+  )
+}
+
+export default Quotes
